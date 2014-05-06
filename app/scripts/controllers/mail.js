cfbd8e1aa8376c6e9bcf5941b77f458bcdb7ef49
demarcusfreemon <demarcusfreemon@Apples-MacBook-Pro-2.local>
2014-05-06 10:56:29 -0700

    framework complete

--- app/scripts/controllers/mail.js
+++ app/scripts/controllers/mail.js
@@ -4,9 +4,19 @@ angular.module('launch29App')
   .controller('MailCtrl', function ($scope, $location, $http) {
   	$scope.mail = {};
 
+  	var defaultForm = {
+              name : "",
+              winery : "",
+              email: "",
+              phone: "",
+              note:""
+          };
+
   	$scope.sendmail = function(){
-  		$http.post('/api/mail', $scope.mail).success(function(data, status, headers) {
+		$http.post('/api/mail', $scope.mail).success(function(data, status, headers) {
       		console.log(data);
+      		$scope.form.$setPristine();
+  			$scope.mail = defaultForm;
   		});
   	};
 });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,25 @@
 'use strict';
 
 angular.module('launch29App')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+  .controller('MainCtrl', function ($scope, $location) {
+    $scope.goWhatWeDo = function (){
+    // set the location.hash to the id of
+    // the element you wish to scroll to.
+    $location.hash('what_we_do');
+ 
+    // call $anchorScroll()
+    $anchorScroll();
+  };
+
+  $scope.goContact = function (){
+    // set the location.hash to the id of
+    // the element you wish to scroll to.
+    $location.hash('contact');
+ 
+    // call $anchorScroll()
+    $anchorScroll();
+  };
+
+
+
   });
